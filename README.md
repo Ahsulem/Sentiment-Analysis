633ec6b88844d66fd6be434a292d54ebf5645560
Ahmad Suleman <ahmadsulemanwahlajutt2003@gmail.com>
2026-08-10 17:25:42 +0500

    "These here are the files Bitchassniggas"

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Sentiment-Analysis
 MLOps pipeline for Sentiment Analysis on Youtube comments
+
+# Commands
+
+conda create -n youtube python=3.14 -y
+conda activate youtube
+pip install -r requirements.txt
+
+dvc init
+dvc repro
+dvc dag
+
+aws configure
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= 'Sentiment-Analysis',
+        version= '0.0.0',
+        author= 'Ahmad Suleman',
+        author_email= 'ahmadsulemanwahlajutt2003@gmail.com',
+        packages= find_packages(),
+        install_requires= []
+)
+    
